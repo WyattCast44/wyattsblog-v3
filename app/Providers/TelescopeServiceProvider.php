77fffa33773992c07c111593ea3e936da98c0981
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-05-17 19:58:00 -0700

    wip

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,6 +21,9 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
+
+            return true;
+            
             if ($this->app->environment('local')) {
                 return true;
             }
