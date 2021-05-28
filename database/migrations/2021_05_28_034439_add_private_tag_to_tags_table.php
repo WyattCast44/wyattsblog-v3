345ad3ae1b6589b159b6f66d2536a5a1978ce5f7
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-05-27 21:06:28 -0700

    wip

--- database/migrations/2021_05_28_034439_add_private_tag_to_tags_table.php
+++ database/migrations/2021_05_28_034439_add_private_tag_to_tags_table.php
@@ -2,8 +2,6 @@
 
 use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class AddPrivateTagToTagsTable extends Migration
 {
